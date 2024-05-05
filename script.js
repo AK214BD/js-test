07f184ad9576fcf010e582f0bde485fb6c851b7b
Md.Nazmul Hasan Anik <annikk.gldr@gmail.com>
2024-05-05 18:40:40 +0600

    js assignment done

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-// 1.How to Write Variables in Javascript? Write at least 3 Variables
-
-// x=3;
-// y=5;
-// z= x+y;
-// console.log(z)
-
-
 // 2. How to Write Variables in Javascript? Write at least 3 Variables
 let firstnumber = 20;
 console.log(firstnumber);
-let secondnumber = 30;
+var secondnumber = 30;
 console.log(secondnumber);
-let thirdnumber = 50;
+const thirdnumber = 50;
 console.log(thirdnumber);
 
 
-// 3. What is the difference between `let, var, and const` variables?
 // 4. Suppose you want to print your name on the console. How do you write that?
+
+console.log("Anik");
+
+
 // 5. Suppose you want to print your name on the alert box. How do you write that?
+
+alert("Anik");
 // 6. Take- two variables, the first number, and the second number, and then print their sum on the console.
+
+
+let num1 = 10;
+let num2 = 20;
+
+// Calculate the sum
+let sum = num1 + num2;
+
+// Print the sum to the console
+console.log(sum);
